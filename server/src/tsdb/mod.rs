bd886cfa5e4df97d9328851ad3276fc63484e78a
rowan <r@fawkes.io>
2023-03-08 09:52:25 -0500

    finalize repr, start on DB

--- server/src/tsdb/mod.rs
+++ server/src/tsdb/mod.rs
@@ -1,8 +1,43 @@
-use tokio::fs::File;
+use std::path::Path;
+
+use tokio::{fs::{File, OpenOptions}, io::AsyncWriteExt};
+use zerocopy::{FromBytes, AsBytes};
 
 mod repr;
+use repr::FPtr;
 
-pub struct DB {
-    
-} 
+pub struct DB<T: FromBytes + AsBytes + Clone + Copy> {
+    file: File,
+    head: FPtr<repr::Year<T>>,
+}
+
+impl<T: FromBytes + AsBytes + Clone + Copy> DB<T> {
+    pub async fn open_new(path: &Path) -> tokio::io::Result<Self> {
+        let file = OpenOptions::new()
+            .create_new(true)
+            .read(true)
+            .write(true)
+            .open(path.with_extension(".tsdb"))
+            .await?;
+        let mut db = Self {
+            file,
+            head: FPtr::null(),//initialized in Self::init
+        };
+        db.init().await?;
+        Ok(db)
+    }
+
+    async fn init(&mut self) -> tokio::io::Result<()> {
+        let mut buf = vec![];
+        buf.extend_from_slice(0xDEADBEEFu32.as_bytes());// addr 0
+        self.file.write_all(&buf).await?;
+        Ok(())
+    }
+
+    pub async fn close(mut self) -> tokio::io::Result<()> {
+        self.file.sync_all().await?;
+        self.file.shutdown().await?;
+        Ok(())
+    }
+}
 
--- server/src/tsdb/repr.rs
+++ server/src/tsdb/repr.rs
@@ -1,13 +1,27 @@
 use std::{marker::PhantomData, mem::{self, MaybeUninit}, fmt::Debug};
-use chrono::{Timelike, NaiveTime};
+use chrono::{Timelike, NaiveTime, Datelike};
 use zerocopy::{FromBytes, AsBytes};
 use static_assertions::const_assert_eq;
 
 /// addr=0 is null, just like normal pointers, and invalid.
 #[repr(transparent)]
 pub struct FPtr<T> {
-    addr: u64,
-    _ty: PhantomData<*const T>
+    pub addr: u64,
+    pub _ty: PhantomData<*const T>
+}
+impl<T> FPtr<T> {
+    pub const fn null() -> Self {
+        Self { addr: 0, _ty: PhantomData }
+    }
+    pub const fn with_addr(addr: u64) -> Self {
+        Self { addr, _ty: PhantomData }
+    }
+    pub const fn is_null(self) -> bool {
+        self.addr == 0
+    }
+    pub const fn pointee_size(self) -> usize {
+        mem::size_of::<T>()
+    }
 }
 impl<T> Debug for FPtr<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -26,40 +40,65 @@ unsafe impl<T> FromBytes for FPtr<T> { fn only_derive_is_allowed_to_implement_th
 unsafe impl<T> AsBytes for FPtr<T> { fn only_derive_is_allowed_to_implement_this_trait() where Self: Sized {} }
 
 
-#[repr(transparent)]
-pub struct Year<D: FromBytes + AsBytes> {
+#[repr(C)]
+pub struct Year<D: FromBytes + AsBytes + Clone + Copy> {
+    pub year: i32,
+    pub _pad0: [u8; 4],
+    /// can be null, null=no more years
+    pub next: FPtr<Year<D>>,
     /// can be null, null=no data for that day
-    days: [FPtr<Day<D>>; 366],
+    pub days: [FPtr<Day<D>>; 366],
 }
-impl<T: FromBytes + AsBytes> Debug for Year<T> {
+const_assert_eq!(
+    mem::size_of::<Year<u128>>(),
+    mem::size_of::<i32>()
+    +mem::size_of::<[u8; 4]>()
+    +mem::size_of::<FPtr<Year<u128>>>()
+    +mem::size_of::<[FPtr<Day<u128>>; 366]>()
+);
+impl<T: FromBytes + AsBytes + Clone + Copy> Year<T> {
+    pub fn with_date(date: impl Datelike) -> Self {
+        Self { year: date.year(), _pad0: [0; 4], next: FPtr::null(), days: [FPtr::null(); 366] }
+    }
+    pub fn has_next(&self) -> bool {
+        !self.next.is_null()
+    }
+}
+impl<T: FromBytes + AsBytes + Clone + Copy> Debug for Year<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Year")
             .field("days", &self.days)
             .finish()
     }
 }
-impl<T: FromBytes + AsBytes> Clone for Year<T> {
+impl<T: FromBytes + AsBytes + Clone + Copy> Clone for Year<T> {
     fn clone(&self) -> Self {
-        Self { days: self.days.clone() }
+        Self {
+            year: self.year,
+            _pad0: self._pad0,
+            next: self.next,
+            days: self.days,
+        }
     }
 }
-unsafe impl<T: FromBytes + AsBytes> FromBytes for Year<T> { fn only_derive_is_allowed_to_implement_this_trait() where Self: Sized {} }
-unsafe impl<T: FromBytes + AsBytes> AsBytes for Year<T> { fn only_derive_is_allowed_to_implement_this_trait() where Self: Sized {} }
+unsafe impl<T: FromBytes + AsBytes + Clone + Copy> FromBytes for Year<T> { fn only_derive_is_allowed_to_implement_this_trait() where Self: Sized {} }
+unsafe impl<T: FromBytes + AsBytes + Clone + Copy> AsBytes for Year<T> { fn only_derive_is_allowed_to_implement_this_trait() where Self: Sized {} }
 
 pub type Day<D> = TimeSegment<D>;
 
 #[repr(C)]
-pub struct TimeSegment<D: FromBytes + AsBytes> {
-    start_time: DayTime,
-    end_time: DayTime,
+pub struct TimeSegment<D: FromBytes + AsBytes + Clone + Copy> {
+    pub start_time: DayTime,
+    pub end_time: DayTime,
     /// can be null, null=no next time segment
     /// there can only be a next time segment
     /// when all entries in this segment are full
-    next: FPtr<Day<D>>,
+    pub next: FPtr<Day<D>>,
     //number of valid entries 
-    len: u16,
-    _pad0: [u8; 6],
+    pub len: u16,
+    pub _pad0: [u8; 6],
     //TODO: optimize this quantity 
+    //NOTE: D must be copy to make this safe to drop
     /// can be null, null=no more entries, and
     /// once one element is null all following ones must also be null.
     /// 
@@ -68,12 +107,25 @@ pub struct TimeSegment<D: FromBytes + AsBytes> {
     /// `[data, data, null, null, null]` <- valid
     ///
     /// `[data, null, null, data, null]` <- invalid
-    entries_time: [MaybeUninit<DayTime>; 512],
+    pub entries_time: [MaybeUninit<DayTime>; 512],
     /// see entries_time
-    entries_data: [MaybeUninit<zerocopy::Unalign<D>>; 512],
+    pub entries_data: [MaybeUninit<zerocopy::Unalign<D>>; 512],
 }
-unsafe impl<T: FromBytes + AsBytes> FromBytes for TimeSegment<T> { fn only_derive_is_allowed_to_implement_this_trait() where Self: Sized {} }
-unsafe impl<T: FromBytes + AsBytes> AsBytes for TimeSegment<T> { fn only_derive_is_allowed_to_implement_this_trait() where Self: Sized {} }
+impl<T: FromBytes + AsBytes + Clone + Copy> TimeSegment<T> {
+    pub fn new_full_day() -> Self {
+        Self {
+            start_time: DayTime::from_chrono(&NaiveTime::from_hms_opt(0, 0, 0).unwrap()),
+            end_time: DayTime::from_chrono(&NaiveTime::from_hms_opt(23, 59, 59).unwrap()),
+            next: FPtr::null(),
+            len: 0,
+            _pad0: [0; 6],
+            entries_time: unsafe { MaybeUninit::uninit().assume_init() },
+            entries_data: unsafe { MaybeUninit::uninit().assume_init() },
+        }
+    }
+}
+unsafe impl<T: FromBytes + AsBytes + Clone + Copy> FromBytes for TimeSegment<T> { fn only_derive_is_allowed_to_implement_this_trait() where Self: Sized {} }
+unsafe impl<T: FromBytes + AsBytes + Clone + Copy> AsBytes for TimeSegment<T> { fn only_derive_is_allowed_to_implement_this_trait() where Self: Sized {} }
 
 const_assert_eq!(
     mem::size_of::<TimeSegment<u128>>(), 
