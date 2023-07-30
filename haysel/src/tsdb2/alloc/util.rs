de2609f59f2ffa30a7aadbab15fd69924c0e8c11
Rowan S-L <r@fawkes.io>
2023-07-30 08:43:47 -0400

    remove test assert

--- haysel/src/tsdb2/alloc/util.rs
+++ haysel/src/tsdb2/alloc/util.rs
@@ -1,10 +1,10 @@
 #[cfg(test)]
 pub mod test;
 
-use std::mem::{self, align_of, size_of};
+use std::mem::{align_of, size_of};
 
 use self::comptime_hacks::{Condition, IsTrue};
-use super::ptr::{Ptr, Void};
+use super::ptr::Ptr;
 use zerocopy::{AsBytes, FromBytes};
 
 pub mod comptime_hacks {
@@ -21,22 +21,27 @@ pub struct ChunkedLinkedList<const N: usize, T: AsBytes + FromBytes> {
     pub data: [T; N],
 }
 
-const _: &'static dyn IsTrue = &Condition::<
-    {
-        mem::size_of::<Ptr<Void>>()
-            + mem::size_of::<u64>()
-            + mem::size_of::<
-                [crate::tsdb2::repr::Station; crate::tsdb2::tuning::STATION_MAP_CHUNK_SIZE],
-            >()
-            == mem::size_of::<
-                ChunkedLinkedList<
-                    { crate::tsdb2::tuning::STATION_MAP_CHUNK_SIZE },
-                    crate::tsdb2::repr::Station,
-                >,
-            >()
-    },
->;
-
+// const _: &'static dyn IsTrue = &Condition::<
+//     {
+//         mem::size_of::<Ptr<Void>>()
+//             + mem::size_of::<u64>()
+//             + mem::size_of::<
+//                 [crate::tsdb2::repr::Station; crate::tsdb2::tuning::STATION_MAP_CHUNK_SIZE],
+//             >()
+//             == mem::size_of::<
+//                 ChunkedLinkedList<
+//                     { crate::tsdb2::tuning::STATION_MAP_CHUNK_SIZE },
+//                     crate::tsdb2::repr::Station,
+//                 >,
+//             >()
+//     },
+// >;
+//
+// this function is a replacement for something like what is above (but in the where clause of ChunkedLinkedList)
+// if you actually put this in there (i think it is the fact that the size of self depends on the const generic N)
+// it will give a `unconstrained generic constant` error, with a very unhelpfull help message.
+//
+// this does limit the types that can go in ChunkedLinkedList, but it should work fine
 #[doc(hidden)]
 pub const fn works<T>() -> bool {
     align_of::<T>() == 8 && size_of::<T>() % 8 == 0
