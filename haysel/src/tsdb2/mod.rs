1223310d6faf7911c2a19b2d0689ac7d5c23abbc
Rowan S-L <r@fawkes.io>
2023-08-10 12:08:48 -0400

    remove incorrect warning

--- haysel/src/tsdb2/mod.rs
+++ haysel/src/tsdb2/mod.rs
@@ -126,7 +126,6 @@ impl<Store: Storage> Database<Store> {
         id: ChannelID,
         kind: repr::DataGroupType,
     ) -> Result<(), DBError<<Store as Storage>::Error>> {
-        warn!("TODO: check that the channel does not already exist");
         let eptr = self.alloc.get_entrypoint().await?.cast::<DBEntrypoint>();
         let entry = Object::new_read(&mut self.alloc, eptr).await?;
         let station = ChunkedLinkedList::find(entry.stations.map, &mut self.alloc, |s| s.id == to)
