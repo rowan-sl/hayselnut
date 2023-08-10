9255a29127aafd6248ef647a2cf91345fa621f74
Rowan S-L <r@fawkes.io>
2023-08-10 12:07:01 -0400

    add test for (part of) add_data

--- haysel/src/tsdb2/test.rs
+++ haysel/src/tsdb2/test.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, NaiveDateTime};
 use tracing_test::traced_test;
 use uuid::Uuid;
 
@@ -94,5 +95,49 @@ async fn create_structure() {
     )
     .await
     .expect("add_channel (sporadic) failed");
+    db.close().await.expect("failed to close database")
+}
+
+#[tokio::test]
+#[traced_test]
+async fn simple_add_data_periodic() {
+    let mut db = get_db().await;
+    let station_id = Uuid::new_v4();
+    db.add_station(station_id)
+        .await
+        .expect("add_station failed");
+    let channel_id = Uuid::new_v4();
+    db.add_channel(
+        station_id,
+        channel_id,
+        crate::tsdb2::repr::DataGroupType::Periodic,
+    )
+    .await
+    .expect("add_channel (periodic) failed");
+
+    // test negative numbers while we are at it
+    let mut __time = -500i64;
+    let mut get_time = || {
+        let x = if rand::random::<f64>() < 0.05 {
+            // large, inconsistant delay
+            120.0 + rand::random::<f32>() * 120.0
+        } else {
+            // small, consistant delay (about 30s with +-4s deviation)
+            30.0 + (rand::random::<f32>() - 0.5) * 4.0
+        };
+        __time += x as i64;
+        DateTime::from_utc(
+            NaiveDateTime::from_timestamp_opt(__time, 0).unwrap(),
+            chrono::Utc,
+        )
+    };
+
+    const ENTRIES: usize = 5_000;
+    for _ in 0..ENTRIES {
+        db.add_data(station_id, channel_id, get_time(), rand::random::<f32>())
+            .await
+            .expect("failed to add entry");
+    }
+
     db.close().await.expect("failed to close database")
 }
