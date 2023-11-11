7ddbc36029fea67c2839c6fd56cbf8b24ddcb2da
Rowan S-L <r@fawkes.io>
2023-11-11 10:46:49 -0500

    impl more stuff

--- haysel/src/tsdb3/mod.rs
+++ haysel/src/tsdb3/mod.rs
@@ -4,16 +4,18 @@
 
 use std::{
     alloc::Layout,
+    cmp::max,
     fs::OpenOptions,
     marker::PhantomData,
-    mem::size_of,
+    mem::{align_of, size_of},
     ops::{DerefMut, Range},
     ptr::slice_from_raw_parts_mut,
 };
 
 use anyhow::Result;
 use memmap2::MmapMut;
-use zerocopy::Ref;
+use static_assertions::const_assert;
+use zerocopy::{AsBytes, FromBytes, FromZeroes, Ref};
 
 use crate::tsdb3::ptr::Ptr;
 
@@ -22,7 +24,7 @@ mod repr;
 
 /// memory address of the start of the data access slice
 #[derive(Clone, Copy)]
-struct BaseOffset<'a>(*const u8, PhantomData<&'a ()>);
+pub struct BaseOffset<'a>(*const u8, PhantomData<&'a ()>);
 
 impl<'a> BaseOffset<'a> {
     pub fn ptr(self) -> *const u8 {
@@ -46,15 +48,18 @@ impl<'a> SlicePtr<'a> for MultipleAccess<'a> {
     }
 }
 
-fn access_memmap<'a>(map: &'a mut MmapMut, t_reg: &TypeRegistry) -> (BaseOffset<'a>, &'a mut [u8]) {
+fn access_memmap<'a>(
+    map: &'a mut MmapMut,
+    alloc_t_reg: &TypeRegistry,
+) -> (BaseOffset<'a>, &'a mut [u8]) {
     let map = map.deref_mut();
-    assert!(map.as_mut_ptr().is_aligned_to(t_reg.max_align()));
+    assert!(map.as_mut_ptr().is_aligned_to(alloc_t_reg.max_align()));
     (BaseOffset(map as *mut [u8] as *const u8, PhantomData), map)
 }
 
 /// Note: this allows multiple accesses, but only once - when a reference to a sub part is dropped,
 ///  it may not be referenced again through this struct
-struct MultipleAccess<'a> {
+pub struct MultipleAccess<'a> {
     len: usize,
     ptr: *mut u8,
     /// (ptr, len)
@@ -123,7 +128,7 @@ impl<'a> MultipleAccess<'a> {
 }
 
 #[derive(Default)]
-struct TypeRegistry {
+pub struct TypeRegistry {
     types: Vec<Layout>,
 }
 
@@ -148,28 +153,54 @@ impl TypeRegistry {
         self.types.iter().map(|t| t.align()).max().unwrap_or(1)
     }
 
+    pub fn min_align(&self) -> usize {
+        self.types.iter().map(|t| t.align()).min().unwrap_or(1)
+    }
+
     pub fn contains_similar<T>(&self) -> bool {
         self.types.contains(&Layout::new::<T>())
     }
 }
 
+#[inline]
+pub(crate) fn round_up_to(n: usize, divisor: usize) -> usize {
+    debug_assert!(divisor.is_power_of_two());
+    (n + divisor - 1) & !(divisor - 1)
+}
+
+pub fn alignment_pad_size<T>() -> usize {
+    // align to the alignment of chunk header or T, whichever is greater
+    // ensures that the next chunk will have the proper alignment for its header,
+    // and that T is properly aligned if its align is greater than that of ChunkHeader
+    if align_of::<T>() > align_of::<repr::ChunkHeader>() {
+        assert!(align_of::<T>() % align_of::<repr::ChunkHeader>() == 0);
+    }
+    round_up_to(
+        size_of::<T>() + size_of::<repr::ChunkHeader>(),
+        max(align_of::<T>(), align_of::<repr::ChunkHeader>()),
+    ) - size_of::<T>()
+        - size_of::<repr::ChunkHeader>()
+}
+
 pub struct AllocAccess<'a> {
-    t_reg: &'a TypeRegistry,
     alloc_t_reg: &'a TypeRegistry,
     base: BaseOffset<'a>,
-    header: Ref<&'a mut [u8], repr::AllocHeader>,
-    free_lists: Ref<&'a mut [u8], [repr::AllocCategoryHeader]>,
+    header: &'a mut repr::AllocHeader,
+    free_lists: &'a mut [repr::AllocCategoryHeader],
     dat: MultipleAccess<'a>,
 }
 
 impl<'a> AllocAccess<'a> {
-    pub fn new(map: &'a mut MmapMut, t_reg: &'a TypeRegistry, alloc_t_reg: &'a TypeRegistry, write_header: bool) -> Self {
+    pub fn new(map: &'a mut MmapMut, alloc_t_reg: &'a TypeRegistry, write_header: bool) -> Self {
+        // make sure that all allocator types are alligned properly
+        const_assert!(align_of::<repr::AllocCategoryHeader>() <= align_of::<repr::AllocHeader>());
+        const_assert!(align_of::<repr::AllocCategoryHeader>() <= align_of::<repr::ChunkHeader>());
         // -- get memmap content --
-        let (base, dat): (BaseOffset, &mut [u8]) = access_memmap(map, &t_reg);
+        let (base, dat): (BaseOffset, &mut [u8]) = access_memmap(map, &alloc_t_reg);
         // -- get header --
         let (mut header, dat) = Ref::<_, repr::AllocHeader>::new_from_prefix(dat).unwrap();
         if write_header {
-            *header = repr::AllocHeader::new(Ptr::null(), t_reg.num_types() as u64);
+            *header = repr::AllocHeader::new(Ptr::null(), alloc_t_reg.num_types() as u64);
         }
         assert!(header.verify());
         // -- get the free lists --
@@ -179,11 +210,10 @@ impl<'a> AllocAccess<'a> {
         )
         .unwrap();
         Self {
-            t_reg,
             alloc_t_reg,
             base,
-            header,
-            free_lists,
+            header: header.into_mut(),
+            free_lists: free_lists.into_mut_slice(),
             dat: MultipleAccess::new(dat),
         }
     }
@@ -192,8 +222,10 @@ impl<'a> AllocAccess<'a> {
         // -- find the appropreate list --
         let (list_header, found) = 'found: {
             for list in &mut *self.free_lists {
-                let &mut repr::AllocCategoryHeader { size, head } = list;
-                if size_of::<T>() as u64 == size {
+                if (size_of::<T>() + alignment_pad_size::<T>()) as u64 == list.size
+                    && align_of::<T>() as u64 == list.align
+                {
+                    let head = list.head;
                     break 'found (list, head);
                 }
             }
@@ -208,7 +240,7 @@ impl<'a> AllocAccess<'a> {
         let first_dat = self
             .dat
             .get(found.localize_to(self.base, &self.dat).to_range_usize());
-        let (first, _) = Ref::<_, repr::ChunkHeader>::new_from_prefix(&mut *first_dat).unwrap();
+        let first = Ref::<_, repr::ChunkHeader>::new(&mut *first_dat).unwrap();
         // -- remove `first` from this free list --
         if first.next.is_null() {
             // no `next` element in the list, so set the head to null
@@ -220,6 +252,62 @@ impl<'a> AllocAccess<'a> {
         self.dat.put(first_dat);
         Some(found)
     }
+
+    /// allocates a new zeroed T, and returns a ref to it
+    pub fn alloc<T: AsBytes + FromBytes + FromZeroes>(&mut self) -> &'a mut T {
+        assert!(self.alloc_t_reg.contains_similar::<T>());
+        if let Some(free_spot) = self.get_free_for::<T>() {
+            let dat = self
+                .dat
+                .get(free_spot.localize_to(self.base, &self.dat).to_range_usize());
+            let (mut header, dat) = Ref::<_, repr::ChunkHeader>::new_from_prefix(dat).unwrap();
+            let mut flags = repr::ChunkFlags::from_bits(header.flags).unwrap();
+            flags.remove(repr::ChunkFlags::FREE);
+            header.flags = flags.bits();
+            // remove alignment padding
+            Ref::<_, T>::new_zeroed(&mut dat[alignment_pad_size::<T>()..])
+                .unwrap()
+                .into_mut()
+        } else {
+            let global_ptr = Ptr::<repr::ChunkHeader>::with(self.header.used);
+            self.header.used += (size_of::<repr::ChunkHeader>()
+                + alignment_pad_size::<T>()
+                + size_of::<T>()) as u64;
+            // -- write the new header --
+            let header_dat = self.dat.get(
+                global_ptr
+                    .localize_to(self.base, &self.dat)
+                    .to_range_usize(),
+            );
+            let mut header = Ref::<_, repr::ChunkHeader>::new(header_dat).unwrap();
+            *header = repr::ChunkHeader {
+                flags: repr::ChunkFlags::empty().bits(),
+                len: (alignment_pad_size::<T>() + size_of::<T>()) as _,
+                // dangling, non null (not required, but it will make detecting errors easier)
+                next: Ptr::with(1),
+            };
+            // -- get and return the body --
+            let dat = self.dat.get(
+                global_ptr
+                    .offset((size_of::<repr::ChunkHeader>() + alignment_pad_size::<T>()) as _)
+                    .cast::<T>()
+                    .to_range_usize(),
+            );
+            Ref::<_, T>::new_zeroed(dat).unwrap().into_mut()
+        }
+    }
+
+    /// returns a ref to an already allocated value
+    pub fn read<T: AsBytes + FromBytes + FromZeroes>(&mut self, ptr: Ptr<T>) -> &'a mut T {
+        assert!(self.alloc_t_reg.contains_similar::<T>());
+        // -- get and return the body --
+        let dat = self.dat.get(
+            ptr.offset((size_of::<repr::ChunkHeader>() + alignment_pad_size::<T>()) as _)
+                .cast::<T>()
+                .to_range_usize(),
+        );
+        Ref::<_, T>::new_zeroed(dat).unwrap().into_mut()
+    }
 }
 
 pub fn main() -> Result<()> {
@@ -228,25 +316,21 @@ pub fn main() -> Result<()> {
         .write(true)
         .create(true)
         .open("test.tsdb3")?;
+    file.set_len(0)?;
     file.set_len(1024 * 500)?;
     // Saftey: lol. lmao.
     let mut map = unsafe { MmapMut::map_mut(&file)? };
     let alloc_t_reg = {
         let mut alloc_t_reg = TypeRegistry::new();
         alloc_t_reg.register::<u64>();
+        alloc_t_reg.register::<[u8; 13]>();
         alloc_t_reg
     };
-    let t_reg = {
-        let mut t_reg = TypeRegistry::new();
-        t_reg.extend(&alloc_t_reg);
-        t_reg.register::<repr::AllocHeader>();
-        t_reg.register::<repr::AllocCategoryHeader>();
-        t_reg.register::<repr::ChunkFlags>();
-        t_reg.register::<repr::ChunkHeader>();
-        t_reg
-    };
     {
-        let alloc = AllocAccess::new(&mut map, &t_reg, &alloc_t_reg true);
+        let mut alloc = AllocAccess::new(&mut map, &alloc_t_reg, true);
+        let v = alloc.alloc::<[u8; 13]>();
+        *v = *b"Hello, World!";
     }
+    file.sync_all()?;
     Ok(())
 }
--- haysel/src/tsdb3/repr.rs
+++ haysel/src/tsdb3/repr.rs
@@ -1,9 +1,11 @@
+use std::mem::size_of;
+
 use bitflags::bitflags;
 use zerocopy::{AsBytes, FromBytes, FromZeroes};
 
 use super::ptr::{Ptr, Void};
 
-pub const MAGIC_BYTES: [u8; 12] = *b"Hayselnut DB";
+pub const MAGIC_BYTES: [u8; 12] = *b"Haysel DB v3";
 
 bitflags! {
     #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
@@ -17,11 +19,12 @@ bitflags! {
 pub struct ChunkHeader {
     /// (ChunkFlags)
     pub flags: u32,
-    /// length of the chunk
+    /// length of the chunk (includes alignment padding)
     pub len: u32,
     /// pointer to the next free chunk (null if last)
     /// - dangling if in use
     pub next: Ptr<ChunkHeader>,
+    // not pictured: alignment padding
 }
 
 #[derive(Clone, Copy, FromBytes, FromZeroes, AsBytes)]
@@ -32,6 +35,8 @@ pub struct AllocHeader {
     /// entrypoint pointer - pointer to something that can be used to get a frame of
     /// reference to the content stored in the allocator
     pub entrypoint: Ptr<Void>,
+    /// size used in the backing store
+    pub used: u64,
     /// the size of the free list (entries, not bytes)
     /// used to make sure that it is read correctly
     pub free_list_size: u64,
@@ -44,6 +49,8 @@ impl AllocHeader {
             magic_bytes: MAGIC_BYTES,
             _padding: [0u8; 4],
             entrypoint,
+            used: (size_of::<Self>() + size_of::<AllocCategoryHeader>() * free_list_size as usize)
+                as _,
             free_list_size,
         }
     }
@@ -57,5 +64,6 @@ impl AllocHeader {
 #[repr(C)]
 pub struct AllocCategoryHeader {
     pub size: u64,
+    pub align: u64,
     pub head: Ptr<ChunkHeader>,
 }
